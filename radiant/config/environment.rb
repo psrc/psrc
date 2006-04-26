ab0c43dc863967f09669cda1915f0ea133344338
jlong <jlong@6226cccd-6db4-da11-80d6-0014220fd306>
2006-04-26 19:55:43 +0000

    created a setup script and updated the README
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@5 6226cccd-6db4-da11-80d6-0014220fd306

--- radiant/config/environment.rb
+++ radiant/config/environment.rb
@@ -41,9 +41,8 @@ Rails::Initializer.run do |config|
   # Make Active Record use UTC-base instead of local time
   config.active_record.default_timezone = :utc
   
-  # Use Active Record's schema dumper instead of SQL when creating the test database
-  # (enables use of different database adapters for development and test environments)
-  # config.active_record.schema_format = :ruby
+  # Make sure plugins are loaded from lib and vendor
+  config.plugin_paths = ['lib/plugins', 'vendor/plugins']
 
   # See Rails::Configuration for more options
 end
