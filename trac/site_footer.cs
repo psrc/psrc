9a0c832931c5472a503fcb7d95c931ed472f0c7b
jlong <jlong@6226cccd-6db4-da11-80d6-0014220fd306>
2006-04-27 20:15:14 +0000

    imported trac templates
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@8 6226cccd-6db4-da11-80d6-0014220fd306

---
+++ trac/site_footer.cs
@@ -0,0 +1,9 @@
+<?cs
+#########################################################################
+# Site footer - Contents are automatically inserted after main Trac HTML
+?>
+<div id="foot">
+  <p>
+    Web Site Design, Logo, Etc. Copyright &copy; 2006, <a href="http://wiseheartdesign.com/">John W. Long</a>. All Rights Reserved.
+  </p>
+</div>
