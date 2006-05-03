92799059576b951e2d05cec0d620e7d372a19c58
jlong <jlong@6226cccd-6db4-da11-80d6-0014220fd306>
2006-05-03 18:54:30 +0000

    repaired problem with behavior generator unit test template
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@21 6226cccd-6db4-da11-80d6-0014220fd306

--- radiant/lib/generators/behavior/templates/unit_test.rb
+++ radiant/lib/generators/behavior/templates/unit_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '<%= '/..' * class_nesting_depth %>/../test_helper'
+require File.dirname(__FILE__) + '<%= '/..' * class_nesting_depth %>/../../test_helper'
 
 class <%= class_name %>BehaviorTest < Test::Unit::TestCase
   # Replace this with your real tests.
