38467d5dbcff40703e3281d6f405cf75f201da6a
jlong <jlong@6226cccd-6db4-da11-80d6-0014220fd306>
2006-05-06 04:22:46 +0000

    updated db/setup.rb to create additional pages demonstrating the archive behavior
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@25 6226cccd-6db4-da11-80d6-0014220fd306

--- radiant/db/setup.rb
+++ radiant/db/setup.rb
@@ -81,12 +81,28 @@ ActiveRecord::Schema.define(:version => 8) do
 
     puts "Create initial layout..."
     layout = Layout.create :name => 'Normal', :content => <<-HTML, :created_at => now, :created_by => admin, :updated_at => now, :updated_by => admin
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
-  <head><title><r:title /></title></head>
+  <head>
+    <title><r:title /></title>
+    <style type="text/css">
+    <!--
+      body {
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        font-size: 80%;
+      }
+    -->
+    </style>
+  </head>
   <body>
     <r:snippet name="header" />
-    <r:content />
-    <r:content part="extended" />
+    <div id="content">
+      <r:content />
+      <div id="extended">
+        <r:content part="extended" />
+      </div>
+    </div>
     <r:snippet name="footer" />
   </body>
 </html>
@@ -94,15 +110,52 @@ ActiveRecord::Schema.define(:version => 8) do
 
     puts "Creating home page..."
     home_page = Page.create :title => 'Home Page', :slug => '/', :breadcrumb => 'Home', :status_id => 100, :parent_id => nil, :layout_id => layout, :created_at => now, :created_by => admin, :updated_at => now, :updated_by => admin
-    PagePart.create :name => 'body', :content => 'This is body content for your home page.', :page => home_page
+    PagePart.create :name => 'body', :content => <<-HTML, :page => home_page
+<r:find url="/articles/">
+<r:children:each limit="5" order="desc">
+<div class="entry">
+  <h3><r:link /></h3>
+  <r:content />
+  <r:if_content part="extended"><r:link anchor="extended">Continue Reading&#8230;</r:link></r:if_content>
+</div>
+</r:children:each>
+</r:find>
+    HTML
+
+    puts "Creating archive page..."
+    archives = Page.create :title => 'Articles', :slug => 'articles', :breadcrumb => 'Articles', :status_id => 100, :parent_id => home_page.id, :layout_id => layout, :created_at => now, :created_by => admin, :updated_at => now, :updated_by => admin, :behavior_id => 'Archive' 
+    PagePart.create :name => 'body', :content => <<-HTML, :page => archives
+<ul>
+<r:children:each order="desc">
+  <li><r:link /></li>
+</r:children:each>
+</ul>
+    HTML
+
+    puts "Creating first post..."
+    first_post = Page.create :title => 'First Post', :slug => 'first-post', :breadcrumb => 'First Post', :status_id => 100, :parent_id => archives.id, :layout_id => layout, :created_at => now, :created_by => admin, :updated_at => now, :updated_by => admin 
+    PagePart.create :name => 'body', :filter_id => 'Textile', :content => <<-HTML, :page => first_post
+This post uses "textile":http://www.textism.com/tools/textile/.
+    HTML
+    
+    puts "Creating second post..."
+    second_post = Page.create :title => 'Second Post', :slug => 'second-post', :breadcrumb => 'Second Post', :status_id => 100, :parent_id => archives.id, :layout_id => layout, :created_at => now, :created_by => admin, :updated_at => now, :updated_by => admin 
+    PagePart.create :name => 'body', :filter_id => 'Markdown', :content => <<-HTML, :page => second_post
+This post uses **Markdown**.
+    HTML
 
     puts "Creating snippets..."
     Snippet.create :name => 'header', :content => <<-HTML
-<h1><r:title /></h1>
+<div id="header">
+  <h1><r:title /></h1>
+</div>
+<hr />
     HTML
     Snippet.create :name => 'footer', :content => <<-HTML
 <hr />
-<p>Powered by <a href="http://radiantcms.org/">Radiant CMS</a></p>
+<div id="footer">
+  <p>Powered by <a href="http://radiantcms.org/">Radiant CMS</a></p>
+</div>
     HTML
 
 end
