d8df515c6edf645a4f93cc8f9874afa7bb50a016
jlong <jlong@6226cccd-6db4-da11-80d6-0014220fd306>
2006-05-10 17:58:13 +0000

    changed version back to 8 in db/schema.rb
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@31 6226cccd-6db4-da11-80d6-0014220fd306

--- radiant/db/schema.rb
+++ radiant/db/schema.rb
@@ -2,7 +2,7 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 8) do
+ActiveRecord::Schema.define(:version => 9) do
 
   create_table "config", :force => true do |t|
     t.column "key", :string, :limit => 40, :default => "", :null => false
