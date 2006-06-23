77d8803cc1ed0f59d2a04e0fdbce3290c8f6b45e
jlong <jlong@6226cccd-6db4-da11-80d6-0014220fd306>
2006-06-23 05:41:12 +0000

    additional tweaks to `radiant` command
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@73 6226cccd-6db4-da11-80d6-0014220fd306

--- radiant/config/environment.rb
+++ radiant/config/environment.rb
@@ -44,7 +44,8 @@ Rails::Initializer.run do |config|
   config.active_record.default_timezone = :utc
   
   # Make sure plugins are loaded from lib and vendor
-  config.plugin_paths = ["#{RAILS_ROOT}/plugins", "#{RADIANT_ROOT}/lib/plugins", "#{RADIANT_ROOT}/vendor/plugins"]
+  config.plugin_paths = ["#{RAILS_ROOT}/plugins", "#{RAILS_ROOT}/vendor/plugins", 
+    "#{RADIANT_ROOT}/lib/plugins", "#{RADIANT_ROOT}/vendor/plugins"]
 
   # See Rails::Configuration for more options
 end
