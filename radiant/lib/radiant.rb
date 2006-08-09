4c9b0cc38c1f6189a5299beb8aade20b1df34bee
jlong <jlong@6226cccd-6db4-da11-80d6-0014220fd306>
2006-08-09 02:53:58 +0000

    * fixed already defined constant warnings for lib/radiant.rb while running in devlopment mode
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@94 6226cccd-6db4-da11-80d6-0014220fd306

--- radiant/lib/radiant.rb
+++ radiant/lib/radiant.rb
@@ -1,16 +1,18 @@
 RADIANT_ROOT = File.join(File.dirname(__FILE__), "..") unless defined? RADIANT_ROOT
 
-module Radiant
-  module Version
-    Major = '0'
-    Minor = '5'
-    Tiny  = '1'
+unless defined? Radiant::Version
+  module Radiant
+    module Version
+      Major = '0'
+      Minor = '5'
+      Tiny  = '1'
     
-    class << self
-      def to_s
-        [Major, Minor, Tiny].join('.')
+      class << self
+        def to_s
+          [Major, Minor, Tiny].join('.')
+        end
+        alias :to_str :to_s
       end
-      alias :to_str :to_s
     end
   end
 end
