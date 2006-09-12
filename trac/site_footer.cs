c3ffb9ffaa8d0fd27b723f93efff14293adbdc49
jlong <jlong@6226cccd-6db4-da11-80d6-0014220fd306>
2006-09-12 04:50:59 +0000

    updated footer to include google analitics stuff
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@117 6226cccd-6db4-da11-80d6-0014220fd306

--- trac/site_footer.cs
+++ trac/site_footer.cs
@@ -7,3 +7,9 @@
     Web Site Design, Logo, Etc. Copyright &copy; 2006, <a href="http://wiseheartdesign.com/">John W. Long</a>. All Rights Reserved.
   </p>
 </div>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-301205-5";
+urchinTracker();
+</script>
