7807f89faaef51196a10494427fa043c75219796
seancribbs <seancribbs@6226cccd-6db4-da11-80d6-0014220fd306>
2007-05-01 17:31:18 +0000

    Prevent generator_base_extension from being auto-loaded when in production mode.  Should alleviate HOME environment issues in some deployment scenarios.
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@420 6226cccd-6db4-da11-80d6-0014220fd306

--- radiant/lib/plugins/extension_patches/init.rb
+++ radiant/lib/plugins/extension_patches/init.rb
@@ -1,5 +1,7 @@
 require 'routing_extension'
 require 'view_paths_extension'
 require 'mailer_view_paths_extension'
-require 'generator_base_extension'
+if RAILS_ENV != 'production'
+  require 'generator_base_extension' 
+end
 require 'fixture_loading_extension'
