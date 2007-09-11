378fed80aa360247753be752de9d5e4e6dd79f51
seancribbs <seancribbs@6226cccd-6db4-da11-80d6-0014220fd306>
2007-09-11 21:49:55 +0000

    Fix issues with misnesting of modules in fixture loading extension and test suites running randomly from dispatchers.
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@482 6226cccd-6db4-da11-80d6-0014220fd306

--- radiant/lib/plugins/extension_patches/init.rb
+++ radiant/lib/plugins/extension_patches/init.rb
@@ -1,4 +1,4 @@
 require 'routing_extension'
 require 'view_paths_extension'
 require 'mailer_view_paths_extension'
-require 'fixture_loading_extension'
+require 'fixture_loading_extension' if RAILS_ENV == 'test'
--- radiant/lib/plugins/extension_patches/lib/fixture_loading_extension.rb
+++ radiant/lib/plugins/extension_patches/lib/fixture_loading_extension.rb
@@ -5,42 +5,41 @@ module Radiant
         include ClassMethods
         alias_method_chain :create_fixtures, :multiple_paths
       end
-    end
-  end
-  
-  module ClassMethods
-    def create_fixtures_with_multiple_paths(fixtures_directory, table_names, class_names = {})
-      table_names = [table_names].flatten.map { |n| n.to_s }
-      connection = block_given? ? yield : ActiveRecord::Base.connection
-      ActiveRecord::Base.silence do
-        fixtures_map = {}
-        fixtures = table_names.map do |table_name|
-          paths = fixtures_directory.dup
-          begin
-            directory = paths.pop
-            fixtures_map[table_name] = Fixtures.new(connection, File.split(table_name.to_s).last, class_names[table_name.to_sym], File.join(directory, table_name))
-          rescue Exception => e
-            retry unless paths.empty?
-            raise e
-          end
-        end               
-        all_loaded_fixtures.merge! fixtures_map  
-  
-        connection.transaction(Thread.current['open_transactions'] == 0) do
-          fixtures.reverse.each { |fixture| fixture.delete_existing_fixtures }
-          fixtures.each { |fixture| fixture.insert_fixtures }
-  
-          # Cap primary key sequences to max(pk).
-          if connection.respond_to?(:reset_pk_sequence!)
-            table_names.each do |table_name|
-              connection.reset_pk_sequence!(table_name)
+    end 
+    module ClassMethods
+      def create_fixtures_with_multiple_paths(fixtures_directory, table_names, class_names = {})
+        table_names = [table_names].flatten.map { |n| n.to_s }
+        connection = block_given? ? yield : ActiveRecord::Base.connection
+        ActiveRecord::Base.silence do
+          fixtures_map = {}
+          fixtures = table_names.map do |table_name|
+            paths = fixtures_directory.dup
+            begin
+              directory = paths.pop
+              fixtures_map[table_name] = Fixtures.new(connection, File.split(table_name.to_s).last, class_names[table_name.to_sym], File.join(directory, table_name))
+            rescue Exception => e
+              retry unless paths.empty?
+              raise e
+            end
+          end               
+          all_loaded_fixtures.merge! fixtures_map  
+    
+          connection.transaction(Thread.current['open_transactions'] == 0) do
+            fixtures.reverse.each { |fixture| fixture.delete_existing_fixtures }
+            fixtures.each { |fixture| fixture.insert_fixtures }
+    
+            # Cap primary key sequences to max(pk).
+            if connection.respond_to?(:reset_pk_sequence!)
+              table_names.each do |table_name|
+                connection.reset_pk_sequence!(table_name)
+              end
             end
           end
+    
+          return fixtures.size > 1 ? fixtures : fixtures.first
         end
-  
-        return fixtures.size > 1 ? fixtures : fixtures.first
-      end
-    end  
+      end  
+    end
   end
 end
 
