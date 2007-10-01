2ed0b0990ff35def3ea8e810eba02a0e75b32917
seancribbs <seancribbs@6226cccd-6db4-da11-80d6-0014220fd306>
2007-10-01 15:41:44 +0000

    Move difference assertions upstream into a test helper.
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@508 6226cccd-6db4-da11-80d6-0014220fd306

--- extensions/forms/test/test_helper.rb
+++ extensions/forms/test/test_helper.rb
@@ -8,7 +8,7 @@ require "#{RADIANT_ROOT}/test/test_helper"
 class Test::Unit::TestCase
   
   # Include a helper to make testing Radius tags easier
-  test_helper :extension_tags
+  test_helper :extension_tags, :difference
   
   # Add the fixture directory to the fixture path
   self.fixture_path << File.dirname(__FILE__) + "/fixtures"
@@ -16,13 +16,5 @@ class Test::Unit::TestCase
   # Add more helper methods to be used by all extension tests here...
   
   # Push this upstream, please
-  def assert_difference(object, method = nil, difference = 1)
-    initial_value = object.send(method)
-    yield
-    assert_equal initial_value + difference, object.send(method), "#{object}##{method}"
-  end
-  def assert_no_difference(object, method, &block)
-    assert_difference object, method, 0, &block
-  end
   
 end
