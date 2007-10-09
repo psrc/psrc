d91e6cda3801e63cd4a2140654d866de987c2aa1
danshep <danshep@6226cccd-6db4-da11-80d6-0014220fd306>
2007-10-09 03:27:43 +0000

    shards: fix tests and make tests robust to the introduction of other extensions
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@546 6226cccd-6db4-da11-80d6-0014220fd306

--- extensions/shards/shards_extension.rb
+++ extensions/shards/shards_extension.rb
@@ -9,21 +9,14 @@ class ShardsExtension < Radiant::Extension
     
   def activate
     ApplicationController.send :helper, Shards::HelperExtensions
+    #helpers aren't automatically inherited by already loaded classes
+    ApplicationController.descendants.each do |controller|
+      controller.send :helper, Shards::HelperExtensions
+    end
     Radiant::AdminUI.class_eval do
       attr_accessor :page #, :snippet, :layout
     end
-    
-    admin.page = OpenStruct.new
-    admin.page.edit = Shards::RegionSet.new do |edit|
-        edit.main.concat %w{edit_header edit_form edit_popups}
-        edit.form.concat %w{edit_title edit_extended_metadata
-                              edit_page_parts}
-        edit.form_bottom.concat %w{edit_buttons}
-        edit.parts_bottom.concat %w{edit_layout_and_type edit_timestamp}
-    end
-    admin.page.index = Shards::RegionSet.new
-    admin.page.remove = Shards::RegionSet.new
-    admin.page.children = Shards::RegionSet.new
+    admin.page = load_default_page_regions
     Admin::PageController.class_eval {
       before_filter :only => :add_part do |c|
         c.send :instance_variable_set, '@template_name', 'edit'
@@ -34,5 +27,21 @@ class ShardsExtension < Radiant::Extension
   def deactivate
     # Need to remove from load and view path! -- APPLY TO CORE
   end
+  
+  private
+    def load_default_page_regions
+        page = OpenStruct.new
+        page.edit = Shards::RegionSet.new do |edit|
+            edit.main.concat %w{edit_header edit_form edit_popups}
+            edit.form.concat %w{edit_title edit_extended_metadata
+                                  edit_page_parts}
+            edit.form_bottom.concat %w{edit_buttons}
+            edit.parts_bottom.concat %w{edit_layout_and_type edit_timestamp}
+        end
+        page.index = Shards::RegionSet.new
+        page.remove = Shards::RegionSet.new
+        page.children = Shards::RegionSet.new   
+        page
+    end
   
 end
--- extensions/shards/test/functional/shards_extension_test.rb
+++ extensions/shards/test/functional/shards_extension_test.rb
@@ -11,15 +11,19 @@ class ShardsExtensionTest < Test::Unit::TestCase
     assert_respond_to admin, :page
     assert_not_nil admin.page
     assert_instance_of OpenStruct, admin.page
+  end
+  
+  def test_page_edit_region_set_default_settings
+    page = ShardsExtension.new.send(:load_default_page_regions)
     %w{edit remove children index}.each do |action|
-      assert_not_nil admin.page.send(action)
-      assert_instance_of Shards::RegionSet, admin.page.send(action)
+      assert_not_nil page.send(action)
+      assert_instance_of Shards::RegionSet, page.send(action)
     end
-    assert_equal %w{edit_header edit_form edit_popups}, admin.page.edit.main
+    assert_equal %w{edit_header edit_form edit_popups}, page.edit.main
     assert_equal %w{edit_title edit_extended_metadata
-                              edit_page_parts edit_layout_and_type 
-                              edit_timestamp}, admin.page.edit.form
-    assert_equal %w{edit_buttons}, admin.page.edit.form_bottom
+                              edit_page_parts}, page.edit.form
+    assert_equal %w{ edit_layout_and_type edit_timestamp}, page.edit.parts_bottom
+    assert_equal %w{edit_buttons}, page.edit.form_bottom
   end
   
   def test_should_add_render_region_helper
