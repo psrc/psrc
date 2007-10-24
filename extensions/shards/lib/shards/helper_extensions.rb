b178779a449a4abdedb6069904f8c30dfb501b15
seancribbs <seancribbs@6226cccd-6db4-da11-80d6-0014220fd306>
2007-10-24 14:36:45 +0000

    Allow passing of partial options to partials in a region in Shards extension.
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@567 6226cccd-6db4-da11-80d6-0014220fd306

--- extensions/shards/lib/shards/helper_extensions.rb
+++ extensions/shards/lib/shards/helper_extensions.rb
@@ -1,9 +1,9 @@
 module Shards::HelperExtensions
-  def render_region(region)
+  def render_region(region, options={})
     @controller_name ||= @controller.controller_name
     @template_name ||= File.basename(@first_render).split(".").last
     admin.send(@controller_name).send(@template_name)[region].compact.map do |partial|
-      render :partial => partial
+      render options.merge(:partial => partial)
     end.join
   end
 end
