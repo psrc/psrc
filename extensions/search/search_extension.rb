84ee6e2754efa0827db444d1014f47bb3a7137e2
seancribbs <seancribbs@6226cccd-6db4-da11-80d6-0014220fd306>
2007-11-06 15:38:55 +0000

    Added installation instructions and example code to the README file in search extension. Closes #565. [Richard Hurt]
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@577 6226cccd-6db4-da11-80d6-0014220fd306

--- extensions/search/search_extension.rb
+++ extensions/search/search_extension.rb
@@ -1,19 +1,14 @@
 class SearchExtension < Radiant::Extension
   version "0.1"
-  description "Provides a page type that allows you to search for pages in Radiant.  Based on Oliver Baltzer's search_behavior."
-  url "http://dev.radiantcms.org/svn/radiant/branches/mental/extensions/search/"
-
-  # define_routes do |map|
-  #   map.connect 'admin/search/:action', :controller => 'admin/asset'
-  # end
-  
+  description %{Provides a page type that allows you to search for pages in 
+                Radiant.  Based on Oliver Baltzer's search_behavior.}
+  url "http://dev.radiantcms.org/"
+ 
   def activate
-    # admin.tabs.add "Search", "/admin/search", :after => "Layouts", :visibility => [:all]
     SearchPage
   end
   
   def deactivate
-    # admin.tabs.remove "Search"
   end
     
 end
