259cb67bb8ef548e6cf0f520580fd84e653b0d44
lorenjohnson <lorenjohnson@6226cccd-6db4-da11-80d6-0014220fd306>
2007-12-20 06:33:21 +0000

    git-svn-id: http://svn.radiantcms.org/radiant/trunk@685 6226cccd-6db4-da11-80d6-0014220fd306

--- radiant/spec/controllers/site_controller_spec.rb
+++ radiant/spec/controllers/site_controller_spec.rb
@@ -1,81 +1,24 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-# NOTE: This was the previous schema 
-# describe SiteController, "routes page requests" do
-# it "should find and render home page" do
-# it "should find a page one level deep" do
-# it "should find a page two levels deep" do
-# it "should show page not found" do
-# it "should redirect to admin if missing root" do
-# it "should parse pages with Radius" do
-# it "should redirect to 404 if page is not published status" do
-# Pending
-#   it "should display draft and hidden pages on dev site" 
-#   it "should be on dev site in config" 
-#   it "should not have no cache header"
+# TODO: More test translation from test_show_page__page_processed on
 
-# TODO: More test translation from test_show_page__page_processed
-
-describe SiteController do
-
+describe SiteController, "in general" do
   scenario :pages
-  
-  it "should render page not found template when a non-existant page is requested" do
-    get :show_page, :url => 'a/non-existant/page'
-    response.headers["Status"].should == "404 Not Found"
-    response.should render_template('site/not_found')
-  end
-  
+
   it "should parse pages with Radius" do
     get :show_page, :url => 'radius'
     response.should be_success
     response.body.should == 'Radius body.'
   end
-  
-  it "should redirect to 404 if page is not published status" do
-    ['draft', 'hidden'].each do |url|
-      get :show_page, :url => url
-      response.headers["Status"].should == "404 Not Found"
-      response.should render_template('site/not_found')
-    end    
-  end
-  
-  it "should render a custom 404 page" 
-  
-  it "should display draft and hidden pages on dev site" do
-    request.host = 'dev.site.com'
-    ['draft', 'hidden'].each do |url|
-      get :show_page, :url => url
-      response.should be_success
-    end
-  end
-  
-  it "won't display dev pages to dev. url if dev site has been specified in config" do
-    controller.config = { 'dev.host' => 'mysite.com'  }       
-    request.host = 'mysite.com'    
-    ['draft', 'hidden'].each do |url|
-      get :show_page, :url => url
-      response.should be_success
-    end
-  end  
-  
-  it "should function like live site because of config" do
-    controller.config = { 'dev.host' => 'mysite.com'  }       
-    request.host = 'dev.site.com'
-    ['draft', 'hidden'].each do |url|
-      get :show_page, :url => url
-      response.headers["Status"].should == "404 Not Found"      
-    end
-  end
-  
-  it "should not have no cache header" do
+
+  it "should not have a no cache header" do
     get :show_page, :url => "/"
     response.headers.keys.should_not include('Cache-Control')
   end
   
 end
 
-describe SiteController, "will find and render" do
+describe SiteController, "given the following pages exists they will be found and rendered" do
 
   scenario :pages
 
@@ -99,18 +42,60 @@ describe SiteController, "will find and render" do
   
 end
 
-describe SiteController, "Given no pages exist" do 
+describe SiteController, "given no pages exist" do 
 
-  # before :all do
-  #   Page.stub!(:find)
-  # end
+  before :all do
+    # Page.stub!(:find_by_url).with(:url, :live).and_return(nil)
+  end
 
-  it "should redirect to admin" 
-  # do
-  #   get :show_page, :url => '/'
-  #   response.should redirect_to(:controller => 'admin/welcome')
-  # end
+  it "should redirect to admin welcome page" do
+    get :show_page, :url => '/'
+    response.should redirect_to(:controller => 'admin/welcome')
+  end
 
 end
 
+describe SiteController, "given a request for an existing but not pubished page" do
+  scenario :pages
+  
+  it "should display the page when a dev site isn't specified in config and the url of the request begins with 'dev.'" do
+    request.host = 'dev.site.com'
+    ['draft', 'hidden'].each do |url|
+      get :show_page, :url => url
+      response.should be_success
+    end
+  end
+
+  it "should display the page when the dev site is specified in config and the request is for that site" do
+    controller.config = { 'dev.host' => 'mysite.com'  }       
+    request.host = 'mysite.com'    
+    ['draft', 'hidden'].each do |url|
+      get :show_page, :url => url
+      response.should be_success
+    end
+  end  
+
+  it "should not display the page for a request url which begins with 'dev.' if a specific site has been set in config" do 
+    controller.config = { 'dev.host' => 'mysite.com'  }       
+    request.host = 'dev.site.com'
+    ['draft', 'hidden'].each do |url|
+      get :show_page, :url => url
+      response.headers["Status"].should == "404 Not Found"      
+    end
+  end
+  
+end
+
+describe SiteController, "given a request for a non-existant page" do
+  scenario :pages
+
+  it "should render the 'page not found' template" do
+    get :show_page, :url => 'a/non-existant/page'
+    response.headers["Status"].should == "404 Not Found"
+    response.should render_template('site/not_found')
+  end
+  
+  it "should render a custom 404 page" 
+
+end
 
