a969b5b1f50f6f54f93887b21ed1e0f9eaf9f03f
seancribbs <seancribbs@6226cccd-6db4-da11-80d6-0014220fd306>
2008-04-04 19:31:21 +0000

    Accents: Add schema, remove ignored files.
    git-svn-id: http://svn.radiantcms.org/radiant/trunk@793 6226cccd-6db4-da11-80d6-0014220fd306

--- accents/db/schema.rb
+++ accents/db/schema.rb
@@ -9,7 +9,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 1) do
+ActiveRecord::Schema.define(:version => 2) do
+
+  create_table "authors", :force => true do |t|
+    t.string   "login"
+    t.string   "email"
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.string   "remember_token"
+    t.datetime "remember_token_expires_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "website"
+    t.string   "identity_url"
+    t.text     "notes"
+  end
 
   create_table "open_id_authentication_associations", :force => true do |t|
     t.integer "issued"
