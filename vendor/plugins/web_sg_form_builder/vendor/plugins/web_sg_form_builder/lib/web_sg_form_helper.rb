8ba8d2cd995e1baadebcc7efe459723c21a3ef40
Jordan Isip <jordanisip@yahoo.com>
2008-08-15 16:02:17 -0700

    Adding confirmation page

--- vendor/plugins/web_sg_form_builder/vendor/plugins/web_sg_form_builder/lib/web_sg_form_helper.rb
+++
@@ -1,6 +0,0 @@
-module WebSgFormHelper
-  def form_for(name, object = nil, options = {}, &proc)
-    options = (options || {}).merge(:builder => WebSgFormBuilder)
-    super
-  end
-end
--- vendor/plugins/web_sg_form_builder/vendor/plugins/web_sg_form_builder/tasks/web_sg_form_builder_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :web_sg_form_builder do
-#   # Task goes here
-# end
