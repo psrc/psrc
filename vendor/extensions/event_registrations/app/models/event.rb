e4da96b1a3e5cbf52c250b75ffe2091c330ab356
Jordan Isip <jordanisip@yahoo.com>
2008-08-15 15:50:18 -0700

    Merging

--- vendor/extensions/event_registrations/app/models/event.rb
+++ vendor/extensions/event_registrations/app/models/event.rb
@@ -1,11 +1,6 @@
 class Event < ActiveRecord::Base
   has_many :event_options
-  
-  def table_options
-     self.event_options.select { |o| o.max_number_of_attendees > 1 }
-  end
-  
-  def individual_options
-     self.event_options.select { |o| o.max_number_of_attendees == 1 }    
-  end
+  delegate :table_options,      :to => :event_options
+  delegate :individual_options, :to => :event_options
+  delegate :max_table_seating,  :to => :event_options
 end
