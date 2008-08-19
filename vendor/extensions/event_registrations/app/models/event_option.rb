efbe94f4ef07439c138825d9ef783952ef9a6c4b
Joe Van Dyk <joe@tanga.com>
2008-08-18 19:56:40 -0700

    Reverting change
    This reverts commit 1f41d6d1ef0690551dd0fa321f42c3d35afe06a3.
    
    Conflicts:

--- vendor/extensions/event_registrations/app/models/event_option.rb
+++ vendor/extensions/event_registrations/app/models/event_option.rb
@@ -1,10 +1,3 @@
 class EventOption < ActiveRecord::Base
   belongs_to :event
-
-  has_finder :table_options,      :conditions => "max_number_of_attendees > 1"
-  has_finder :individual_options, :conditions => "max_number_of_attendees = 1"
-
-  def self.max_table_seating
-    self.table_options.find(:first).max_number_of_attendees
-  end
 end
