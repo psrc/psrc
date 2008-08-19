8f511e3d893c50e7b448ebc8a3e966f8dbdf0272
Jordan Isip <jordanisip@yahoo.com>
2008-08-19 11:00:29 -0700

    Fixed up the broken code

--- vendor/extensions/event_registrations/app/models/event.rb
+++ vendor/extensions/event_registrations/app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   has_many :event_options
-  
+  delegate :max_table_seating, :to => :event_options
   def table_options
      self.event_options.select { |o| o.max_number_of_attendees > 1 }
   end
--- vendor/extensions/event_registrations/app/models/event_option.rb
+++ vendor/extensions/event_registrations/app/models/event_option.rb
@@ -1,3 +1,6 @@
 class EventOption < ActiveRecord::Base
   belongs_to :event
+  def self.max_table_seating
+    find(:first, :order => 'max_number_of_attendees desc').max_number_of_attendees
+  end
 end
