907d73cfd8f0a7f7ff5ac4e45a797d0d0780bfe3
Joe Van Dyk <joe@tanga.com>
2008-08-19 15:36:07 -0700

    Payment model should be complete

--- vendor/extensions/event_registrations/app/models/payment.rb
+++ vendor/extensions/event_registrations/app/models/payment.rb
@@ -3,26 +3,44 @@ class Payment
 
   def initialize card_values, amount
     @card = ActiveMerchant::Billing::CreditCard.new card_values
+    raise @card.errors.full_messages.join(" and ") unless @card.valid?
     @amount = amount
-    start
+    queue
   end
 
   def completed?
-    @count ||= 0
-    @count += 1
-    if @count > 4
-      return true
-    else
-      return false
-    end
+    @job.finished? and @job.exit_status == 0
   end
 
   def error?
+    @job.finished? and @job.exit_status != 0
+  end
+
+  def error_message
+    @job.stderr.chomp
+  end
+
+  # Load purchase object from STDIN, decode/unmarshal it, and execute purchase
+  def self.start_purchase
+    purchase = Marshal.load(Base64.decode64(STDIN.read))
+    purchase.execute_purchase
+  end
+
+  # Authorize purchase from gateway
+  def execute_purchase
+    attempt = @@gateway.purchase((@amount*100).to_i, @card)
+    if attempt.success?
+      exit 0
+    else
+      STDERR.puts attempt.message
+      exit -1
+    end
   end
 
   private
 
-  def start
-    # submit job to bj
+  def queue
+    stdin = Base64.encode64(Marshal.dump(self))
+    @job = Bj.submit("./script/runner Payment.start_purchase", :stdin => stdin).first
   end
 end
