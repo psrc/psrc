da0b8c8a37f1bc0cf41cf6612042b4765d456388
Joe Van Dyk <joe@tanga.com>
2008-08-21 12:57:57 -0700

    Change email

--- vendor/extensions/event_registrations/app/models/emailer.rb
+++ vendor/extensions/event_registrations/app/models/emailer.rb
@@ -1,5 +1,5 @@
 class Emailer < ActionMailer::Base
-  PSRC_CONTACT = "joe@tanga.com"
+  PSRC_CONTACT = "awerfelmann@psrc.org"
   def registration_confirmation registration
     @recipients = [registration.registration_contact.email, PSRC_CONTACT]
     @subject = "Thanks for registering for the #{ registration.event.name } event!"
