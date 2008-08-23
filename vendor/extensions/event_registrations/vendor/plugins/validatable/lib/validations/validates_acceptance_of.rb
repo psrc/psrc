49286da7dc36425f16ecf1c08617fd9e4e088885
Joe Van Dyk <joe@tanga.com>
2008-08-23 11:36:21 -0700

    Moved plugins into extension

---
+++ vendor/extensions/event_registrations/vendor/plugins/validatable/lib/validations/validates_acceptance_of.rb
@@ -0,0 +1,11 @@
+module Validatable
+  class ValidatesAcceptanceOf < ValidationBase #:nodoc:
+    def valid?(instance)
+      instance.send(self.attribute) == "true" || instance.send(self.attribute) == "1"
+    end
+
+    def message(instance)
+      super || "must be accepted"
+    end
+  end
+end
