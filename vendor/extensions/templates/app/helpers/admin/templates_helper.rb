7eb9bdc0f8a5e999fb942a75f612bf35484b5663
Jordan Isip <jordanisip@yahoo.com>
2008-08-25 12:29:55 -0700

    Modifying template layout

--- vendor/extensions/templates/app/helpers/admin/templates_helper.rb
+++ vendor/extensions/templates/app/helpers/admin/templates_helper.rb
@@ -2,6 +2,6 @@ module Admin::TemplatesHelper
   def page_header title
     html = ""
     html << "<div class='back'>#{ link_to "&laquo; Back to all templates", templates_path }</div"
-    html << "<h2>#{title}</h2>"
+    html << "<h2 id='page-header''>#{title}</h2>"
   end
 end
