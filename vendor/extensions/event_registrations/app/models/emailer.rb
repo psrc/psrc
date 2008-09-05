9128e84430f651e62da83f7aa27609473c829d16
Jordan Isip <jordanisip@yahoo.com>
2008-09-05 13:10:26 -0700

    Adding a from line on emailer

--- vendor/extensions/event_registrations/app/models/emailer.rb
+++ vendor/extensions/event_registrations/app/models/emailer.rb
@@ -1,6 +1,7 @@
 class Emailer < ActionMailer::Base
-  PSRC_CONTACT = "joe@pinkpucker.net"
+  PSRC_CONTACT = "PSRC Registration <joe@pinkpucker.net>"
   def registration_confirmation registration
+    @from = PSRC_CONTACT
     @recipients = [registration.registration_contact.email, PSRC_CONTACT]
     @subject = "#{ registration.event.name } Registration Confirmation!"
     body :registration => registration
