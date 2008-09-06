ca9cc5c91f07bc0da62d278ecf84811e2608e2b0
Jordan Isip <jordanisip@yahoo.com>
2008-09-05 17:35:51 -0700

    Adding PSRC system e-mail address

--- vendor/extensions/event_registrations/app/models/emailer.rb
+++ vendor/extensions/event_registrations/app/models/emailer.rb
@@ -1,7 +1,9 @@
 class Emailer < ActionMailer::Base
+  PSRC_SYSTEM = "PSRC Registration <economicdevelopment@psrc.org>"
   PSRC_CONTACT = "PSRC Registration <joe@pinkpucker.net>"
+  # PSRC_CONTACT = "Sylvia Nelson <snelson@psrc.org>"
   def registration_confirmation registration
-    @from = PSRC_CONTACT
+    @from = PSRC_SYSTEM
     @recipients = [registration.registration_contact.email, PSRC_CONTACT]
     @subject = "#{ registration.event.name } Registration Confirmation!"
     body :registration => registration
