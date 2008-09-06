9a8af2e3acd4e5590bb13f41bce3cccd1fca9912
Joe Van Dyk <joe@tanga.com>
2008-09-05 19:26:05 -0700

    Don't show email link

--- vendor/extensions/event_registrations/app/models/event_attendee.rb
+++ vendor/extensions/event_registrations/app/models/event_attendee.rb
@@ -2,7 +2,7 @@ class EventAttendee < ActiveRecord::Base
   belongs_to :registration_group
 
   def to_s
-    result = "#{name} (<a href='mailto:#{email}'>#{email}</a>)"
+    result = "#{name} <#{email}>"
     result += " of #{ organization }" if organization
     result += " (V)" if vegetarian
     result
