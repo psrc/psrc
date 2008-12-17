a72ef43f8f7578237d91067191bb16877aa29859
Joe Van Dyk <joe@fixieconsulting.com>
2008-12-17 09:52:47 -0800

    Upgraded redcloth to 4

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ end
 require File.join(File.dirname(__FILE__), 'boot')
 
 require 'radius'
+require 'redcloth'
 
 Radiant::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
