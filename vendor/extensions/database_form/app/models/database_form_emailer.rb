bb3b67405425473b67316f54cdcd6bb1d239b270
Joe Van Dyk <joe@fixieconsulting.com>
2009-02-16 17:53:17 -0800

    add from form address

--- vendor/extensions/database_form/app/models/database_form_emailer.rb
+++ vendor/extensions/database_form/app/models/database_form_emailer.rb
@@ -1,6 +1,7 @@
 class DatabaseFormEmailer < ActionMailer::Base
 
   def form_submission form, response
+    from "form-submitter@example.com"
     @recipients   = form.form_email
     @subject      = "#{ form.form_name} form submitted"
     body :form => form, :response => response
