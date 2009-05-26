c2f24dba91e14cdb2e3817174c52bdfb72e746fe
Joe Van Dyk <joe@fixieconsulting.com>
2009-05-26 14:17:17 -0700

    Updated db files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,108 +1,33 @@
-# Please install the Engine Yard Capistrano gem
-# gem install eycap --source http://gems.engineyard.com
+set :application, "psrc"
 
-require "eycap/recipes"
-
-# =============================================================================
-# ENGINE YARD REQUIRED VARIABLES
-# =============================================================================
-# You must always specify the application and repository for every recipe. The
-# repository must be the URL of the repository you want this recipe to
-# correspond to. The :deploy_to variable must be the root of the application.
-
-set :keep_releases,       5
-set :application,         "psrc"
-set :repository,          "github-psrc:joevandyk/psrc.git"
-set :user,                "tanga"
-set :deploy_to,           defer { "/data/#{application}" }
-set :password,            "k31bv4j3"
-set :deploy_via,           :remote_cache
-
-# This will execute the Git revision parsing on the *remote* server rather than locally
-set :real_revision, 			lambda { source.query_revision(revision) { |cmd| capture(cmd) } }
-
-set :monit_group,         "psrc"
-set :scm,                 :git
-set :runner,							"tanga"
-
-set :production_database, "psrc_production"
-set :production_dbhost,   "psql82-2-master"
-
-set :draft_database, "psrc_draft"
-set :draft_dbhost,   "psql82-2-master"
-
-set :dbuser,        "tanga_db"
-set :dbpass,        "lb31v12j"
-
-# comment out if it gives you trouble. newest net/ssh needs this set.
-ssh_options[:paranoid] = false
-
-
-# =============================================================================
-# ROLES
-# =============================================================================
-# You can define any number of roles, each of which contains any number of
-# machines. Roles might include such things as :web, or :app, or :db, defining
-# what the purpose of each machine is. You can also specify options that can
-# be used to single out a specific subset of boxes in a particular role, like
-# :primary => true.
-
-  
-  
-task :production do
-
-  role :web, "74.201.254.36:8235" # tanga_staging [thin,memcached,backgroundrb] [psql82-staging-1] and psrc [thin] [psql82-2-master]
-  role :app, "74.201.254.36:8235", :thin => true, :memcached => true, :backgroundrb => true
-  role :db , "74.201.254.36:8235", :primary => true
-  
-  
-  set :rails_env, "production"
-  set :environment_database, defer { production_database }
-  set :environment_dbhost, defer { production_dbhost }
-end
-
-
-task :draft do
-
-  role :web, "74.201.254.36:8235" # tanga_staging [thin,memcached,backgroundrb] [psql82-staging-1] and psrc [thin] [psql82-2-master]
-  role :app, "74.201.254.36:8235", :thin => true, :memcached => true, :backgroundrb => true
-  role :db , "74.201.254.36:8235", :primary => true
-  
-  
+task :staging do
   set :rails_env, "staging"
-  set :application, "psrc_draft"
-  set :monit_group, "psrc_draft"
-  
-  set :environment_database, defer { draft_database }
-  set :environment_dbhost, defer { draft_dbhost }
+  set :deploy_to, "/data/psrc-staging"
 end
 
-after "deploy:symlink_configs", "psrc_custom"
-task :psrc_custom, :roles => :app, :except => {:no_release => true, :no_symlink => true} do
-  run <<-CMD
-     ln -nfs #{shared_path}/page_attachments #{release_path}/public/page_attachments
-   CMD
-  run <<-CMD
-     ln -nfs #{shared_path}/assets #{release_path}/public/assets
-   CMD
-  run <<-CMD
-     ln -nfs #{shared_path}/uploads #{release_path}/public/uploads
-   CMD
+task :production do
+  set :rails_env, "production"
+  set :deploy_to, "/data/psrc"
 end
 
-# Do not change below unless you know what you are doing!
+set :repository,  "git@github.com:joevandyk/psrc.git"
+set :scm, :git
+set :ssh_options, { :forward_agent => true }
+set :user, 'monkey'
+set :deploy_via, :remote_cache
+set :use_sudo, false
+role :app, "ec2.fixieconsulting.com"
+role :web, "ec2.fixieconsulting.com"
+role :db,  "ec2.fixieconsulting.com", :primary => true
 
 
-task "extensions_migrations", :roles => :db, :except => {:no_release => true, :no_symlink => true} do
-  run <<-CMD
-    cd #{release_path} && RAILS_ENV=#{rails_env} rake db:migrate:extensions
-  CMD
+namespace :deploy do
+  task :restart, :roles => :app do
+    invoke_command "touch #{release_path}/tmp/restart.txt"
+  end
+
+  task :after_update_code, :roles => [:app] do
+    run "ln -nfs #{release_path}/config/database.postgresql.yml #{ release_path }/config/database.yml"
+    run "ln -nfs #{shared_path}/assets #{ release_path }/public/assets"
+  end
 end
-
-after "deploy", "deploy:cleanup"
-after "deploy:migrations" , "deploy:cleanup"
-after "deploy:update_code", "deploy:symlink_configs"
-after "deploy:migrate", "extensions_migrations"
-
-# uncomment the following to have a database backup done before every migration
-# before "deploy:migrate", "db:dump"
