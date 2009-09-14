900fa53e0006a64472687724d89e1a3e151d172e
Joe Van Dyk <joe@fixieconsulting.com>
2009-09-14 13:09:53 -0700

    Redirect to live site

--- vendor/extensions/event_registrations/app/controllers/events_controller.rb
+++ vendor/extensions/event_registrations/app/controllers/events_controller.rb
@@ -4,6 +4,7 @@ class EventsController < ApplicationController
   layout :load_layout
 
   def show
+    redirect_to "http://secure.psrc.org/events/#{params[:id]}" and return unless params[:stay_here]
     @event = Event.find params[:id]
     @progress_step = 1
     session[:registration] = nil
