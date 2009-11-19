6aeca5fd6847c0ee50992af5e9f91cdad41bf5db
Joe Van Dyk <joe@fixieconsulting.com>
2009-11-19 11:16:08 -0800

    Changed email address that form submissions come from

--- vendor/extensions/database_form/app/models/database_form_emailer.rb
+++ vendor/extensions/database_form/app/models/database_form_emailer.rb
@@ -1,7 +1,7 @@
 class DatabaseFormEmailer < ActionMailer::Base
 
   def form_submission form, response
-    from "form-submitter@example.com"
+    from "form-submitter@psrc.org"
     @recipients   = form.form_email
     @subject      = "#{ form.form_name} form submitted"
     body :form => form, :response => response
