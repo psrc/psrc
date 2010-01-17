4e1375ff581279b1634635fe8f6eade455ca6494
Joe Van Dyk <joe@fixieconsulting.com>
2010-01-17 11:03:17 -0800

    modified deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 set :application, "psrc"
+set :branch, "master"
 
 task :staging do
   set :rails_env, "staging"
@@ -10,11 +11,11 @@ task :production do
   set :deploy_to, "/data/psrc"
 end
 
-set :repository,  "git@git.fixiconsulting.com:psrc.git"
+set :repository,  "git@git.fixieconsulting.com:psrc.git"
 set :scm, :git
 set :ssh_options, { :forward_agent => true }
 set :user, 'monkey'
-set :deploy_via, :remote_cache
+#set :deploy_via, :remote_cache
 set :use_sudo, false
 role :app, "ec2.fixieconsulting.com"
 role :web, "ec2.fixieconsulting.com"
@@ -23,11 +24,15 @@ role :db,  "ec2.fixieconsulting.com", :primary => true
 
 namespace :deploy do
   task :restart, :roles => :app do
-    invoke_command "touch #{release_path}/tmp/restart.txt"
+    invoke_command "touch #{current_path}/tmp/restart.txt"
   end
 
   task :after_update_code, :roles => [:app] do
-    run "ln -nfs #{release_path}/config/database.postgresql.yml #{ release_path }/config/database.yml"
-    run "ln -nfs #{shared_path}/assets #{ release_path }/public/assets"
+    run "ln -nfs #{current_path}/config/database.postgresql.yml #{ current_path }/config/database.yml"
+    run "ln -nfs #{shared_path}/assets #{ current_path }/public/assets"
   end
+
+  task :update_code, :except => { :no_release => true } do
+    run "cd #{current_path}; git fetch origin; git reset --hard origin/#{branch}"
+  end 
 end
