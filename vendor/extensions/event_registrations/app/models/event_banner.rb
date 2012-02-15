e01f34946c74e6a61defb7f703e2acd0d7e4beb1
Joshua Wood <josh@hintmedia.com>
2012-02-14 16:01:05 -0800

    Revert "added banners for events"
    This reverts commit 77b19a5854d0f3dba0adce8c013c1687a0e83b19.

--- vendor/extensions/event_registrations/app/models/event_banner.rb
+++
@@ -1,6 +0,0 @@
-class EventBanner < ActiveRecord::Base
-  has_attachment :storage => :file_system, 
-                 :path_prefix => 'public/system/event_banners',
-                 :max_size => 10.megabytes, :processor => :rmagick
-  belongs_to :event
-end
