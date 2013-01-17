589da09842be71d31639409bb1e6f74da679e642
Joshua Wood <josh@hintmedia.com>
2013-01-17 15:14:06 -0800

    Cache feeds for 6 hours, always re-fetch

--- vendor/extensions/feed_reader/lib/feed_cache.rb
+++ vendor/extensions/feed_reader/lib/feed_cache.rb
@@ -3,16 +3,24 @@ require 'feedzirra'
 
 class FeedCache
   include Simpleton
-  
+
   attr_accessor :cache_dir
-  
+
   def get(url)
     FileUtils.mkdir_p(cache_dir) unless File.directory?(cache_dir)
-    if cache_exists?(url)
+
+    # Caches full feed
+    #
+    # This used to attempt to update the cached feed like so:
+    #
+    #   feed = Feedzirra::Feed.update(feed)
+    #
+    # which worked fine on ruby 1.8.7, but returned an empty array on ree.
+    #
+    # It should be fine to re-fetch the entire feed as long as it's being
+    # cached for a long period of time (6 hours in this case).
+    if cache_exists?(url) && !update_cache?(url)
       feed = load_from_cache(url)
-      feed = Feedzirra::Feed.update(feed)
-      clean_duplicates(feed)
-      save_to_cache(url, feed)
       feed
     else
       feed = Feedzirra::Feed.fetch_and_parse(url)
@@ -20,44 +28,36 @@ class FeedCache
       feed
     end
   end
-  
+
   private
-    def cache_file(url)
-      File.join(cache_dir, cache_key(url))
+
+  def cache_file(url)
+    File.join(cache_dir, cache_key(url))
+  end
+
+  def cache_exists?(url)
+    File.exist?(cache_file(url))
+  end
+
+  def update_cache?(url)
+    File.mtime(cache_file(url)) < 6.hours.ago
+  end
+
+  def cache_key(url)
+    Digest::SHA1.hexdigest(url)
+  end
+
+  def load_from_cache(url)
+    Marshal.load(File.read(cache_file(url)))
+  end
+
+  def save_to_cache(url, object)
+    unless object.is_a?(Fixnum)
+      File.open(cache_file(url), "w") {|f| f.write Marshal.dump(object) }
     end
-    
-    def cache_exists?(url)
-      File.exist?(cache_file(url))
-    end
-    
-    def cache_key(url)
-      Digest::SHA1.hexdigest(url)
-    end
-    
-    def clean_duplicates(feed)
-      unless feed.is_a?(Fixnum)
-        new_entries = feed.entries.inject([]) do |collection, entry|
-          if collection.any? {|e| e.url == entry.url }
-            collection
-          else
-            collection << entry
-          end
-        end
-        feed.entries.replace new_entries
-      end
-    end
-    
-    def load_from_cache(url)
-      Marshal.load(File.read(cache_file(url)))
-    end
-    
-    def save_to_cache(url, object)
-      unless object.is_a?(Fixnum)
-        File.open(cache_file(url), "w") {|f| f.write Marshal.dump(object) }
-      end
-    end
-    
-    def initialize
-      @cache_dir = "#{RAILS_ROOT}/tmp/feed_cache"
-    end
+  end
+
+  def initialize
+    @cache_dir = "#{RAILS_ROOT}/tmp/feed_cache"
+  end
 end
